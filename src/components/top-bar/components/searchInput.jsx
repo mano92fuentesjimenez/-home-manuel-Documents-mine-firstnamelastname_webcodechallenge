8655a68c584c4236e78333a0a495a84cf95e62b0
miggy92 <m92fuentes@gmail.com>
2019-09-17 16:35:57 -0400

    added animation for searching

--- src/components/top-bar/components/searchInput.jsx
+++ src/components/top-bar/components/searchInput.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './searchInput.scss';
+import classnames from 'classnames';
+
 export const SearchInput = ({ value, onChange, onEnterPressed })=> {
+  const [focus, setFocus] = useState(false);
   const onInputChange = (e) => onChange(e.target.value);
   const onKeyUp = (e) => {
     if (e.key === 'Enter') {
       onEnterPressed(value);
     }
   };
-  return <input className="search-input" type='text' onChange={onInputChange} value={value} onKeyUp={onKeyUp}/>
+  return <input
+    className={classnames('search-input', {'search-input-writing': focus})}
+    type='text'
+    onChange={onInputChange}
+    value={value}
+    onKeyUp={onKeyUp}
+    onFocus={() => setFocus(true)}
+    onBlur={() => setFocus(false)}
+  />
 };
