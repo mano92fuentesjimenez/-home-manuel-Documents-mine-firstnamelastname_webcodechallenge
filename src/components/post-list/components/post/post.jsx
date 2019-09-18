adf670317c3b4c60b9ba4bde37591e4cd809154c
miggy92 <m92fuentes@gmail.com>
2019-09-18 16:21:25 -0400

    post done

--- src/components/post-list/components/post/post.jsx
+++ src/components/post-list/components/post/post.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 import { ProfilePicture } from "../profilePicture/profilePicture";
+import { Author } from "../author/author";
+import { Title } from "../title/title";
+import { Comments } from "../comments/comments";
+import { UpVotes } from "../up-votes/up-votes";
+import { DownVotes } from "../down-votes/down-votes";
 import './post.scss'
 
 export const Post = ({ post }) => {
     return <div className='post-container'>
-      <ProfilePicture photoUrl={post.thumbnail} classNames='float-left'/>
-      <li>{post.title}</li>
+      <ProfilePicture photoUrl={post.thumbnail}/>
+      <div className='post-text-container'>
+        <Author post={post}/>
+        <Title post={post}/>
+        <div className='post-bottom-bar'>
+          <Comments post={post}/>
+          <UpVotes post={post}/>
+          <DownVotes post={post}/>
+        </div>
+      </div>
+
     </div>
 };
