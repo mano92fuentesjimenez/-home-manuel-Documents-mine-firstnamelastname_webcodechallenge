f716df1e362308ffd19e57ed511f1759b07b5d47
miggy92 <m92fuentes@gmail.com>
2019-09-18 17:32:41 -0400

    added selection of post

--- src/components/post-list/components/post/post.jsx
+++ src/components/post-list/components/post/post.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import { ProfilePicture } from "../profilePicture/profilePicture";
 import { Author } from "../author/author";
 import { Title } from "../title/title";
 import { Comments } from "../comments/comments";
 import { UpVotes } from "../up-votes/up-votes";
 import { DownVotes } from "../down-votes/down-votes";
+import classnames from 'classnames';
 import './post.scss'
 
 export const Post = ({ post }) => {
-    return <div className='post-container'>
+  const [mouseEntered, changeMouseEntered] = useState(false);
+
+    return <div
+      className={classnames('post-container', { 'post-mouse-in': mouseEntered})}
+      onMouseEnter={() => changeMouseEntered(true)}
+      onMouseLeave={() => changeMouseEntered(false)}
+    >
       <ProfilePicture photoUrl={post.thumbnail}/>
       <div className='post-text-container'>
         <Author post={post}/>
