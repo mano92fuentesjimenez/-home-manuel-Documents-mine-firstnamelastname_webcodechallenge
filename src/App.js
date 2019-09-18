ce2badf8a3e1baf278a4689766beba58c765d058
miggy92 <m92fuentes@gmail.com>
2019-09-18 18:28:49 -0400

    added redux dev_tool compatibility

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import TopBar from './components/top-bar';
 import { Provider } from 'react-redux'
 import reducers from './reducers';
-import {combineReducers, createStore, applyMiddleware} from "redux";
+import {combineReducers, createStore, applyMiddleware, compose} from "redux";
 import createSagaMiddleware from 'redux-saga';
 import sagas from './sagas';
 import api from './api';
@@ -15,7 +15,11 @@ const reducer = combineReducers(reducers);
 
 function App() {
   const sagaMiddleware = createSagaMiddleware();
-  const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+  const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+  const enhancer = composeEnhancers(applyMiddleware(sagaMiddleware));
+  const store = createStore(reducer, enhancer);
   const services = {
     api,
   };
