51dbb4c3a74c4c68e2f65503dd2c3776f5d457f6
miggy92 <m92fuentes@gmail.com>
2019-09-19 15:24:28 -0400

    added onHover style

--- src/components/post-action/components/action-card/actionCard.jsx
+++ src/components/post-action/components/action-card/actionCard.jsx
@@ -4,11 +4,14 @@ import './actionCard.scss';
 import { useDrop } from "react-dnd";
 
 export const ActionCard = ({imgPath, actionName, actionText}) => {
-  const [a, drop] = useDrop({
+  const [{ hover }, drop] = useDrop({
     accept: 'post',
+    collect: monitor => ({
+      hover: monitor.isOver({shallow: true}),
+    }),
   });
 
-  return <div ref={drop} className={classnames("post-card-container", actionName, "action-card-container")}>
+  return <div ref={drop} className={classnames("post-card-container", actionName, "action-card-container", {'action-card-hover': hover})}>
     <img src={imgPath} />
     <span>{actionText}</span>
   </div>
